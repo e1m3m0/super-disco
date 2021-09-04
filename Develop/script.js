1064c0759a194ed1c13851fd7f99d66e2e0c398d
e1m3m0 <gamateo@gmail.com>
2021-09-03 21:18:18 -0400

    use moment.js to update description div

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,59 @@
-var schedule = {};
+var schedule = [];
+var now = moment().format("dddd, MMMM Do YYYY");
+var currentDay = $("#currentDay");
+var containerTime = $(".description");
+
+var scheduleTime = function () {
+  var date = moment();
+  var format = "dddd, MMMM Do YYYY";
+  var results = date.format(format);
+  $(currentDay).text(results);
+  console.log(results);
+
+  console.log(containerTime);
+
+  $(containerTime).each(function (element, index) {
+    var time = moment().hour($(this).attr("id"));
+    console.log(time);
+    if (moment(time).isBefore(date)) {
+      $(containerTime).removeClass("present");
+      $(containerTime).removeClass("future");
+      $(containerTime).addClass("past");
+    } else if (moment(time).isSame(date)) {
+      $(containerTime).addClass("present");
+      $(containerTime).removeClass("future");
+      $(containerTime).removeClass("past");
+    } else if (moment(time).isAfter(date)) {
+      $(containerTime).removeClass("present");
+      $(containerTime).addClass("future");
+      $(containerTime).removeClass("past");
+    }
+  });
+};
 
 $(".description").on("click", "p", function () {
   var text = $(this).text().trim();
+  var hour = $(this).attr("class");
 
-  var textInput = $("<textarea>").val(text);
+  console.log(hour);
+
+  var textInput = $("<textarea>").val(text).attr("class", hour);
   $(this).replaceWith(textInput);
 
   textInput.trigger("focus");
 });
 
 $(".description").on("blur", "textarea", function () {
-  var tempSchd = [];
-  var desc = $("textarea").val();
-  var hour = $(".description").attr("id");
-
-  tempSchd.push({
-    hour: hour,
-    desc: desc,
-  });
-
-  schedule = tempSchd;
+  var h = $(this).attr("class");
+  var d = $(this).val();
+  if (!d) {
+    console.log("Did not write a task");
+  } else {
+    schedule.push({
+      hour: h,
+      desc: d,
+    });
+  }
 });
 
 var saveSchedule = function () {
@@ -29,24 +63,23 @@ var saveSchedule = function () {
 var loadSchedule = function () {
   schedule = JSON.parse(localStorage.getItem("schedule"));
 
+  scheduleTime();
+
   if (!schedule) {
-    schedule = {
-      hour: [],
-      desc: [],
-    };
+    schedule = [];
   }
+
   $.each(schedule, function (index, value) {
     console.log(schedule);
     var h = schedule[index].hour;
     var d = schedule[index].desc;
     console.log(h);
-    $("#" + h)
-      .find("p")
-      .text(d);
+    $("." + h).text(d);
     console.log(d);
   });
 };
 
 $(".row").on("click", ".saveBtn", saveSchedule);
 
+setInterval(scheduleTime, 12000000);
 loadSchedule();
